2287fdd6f942e0e508260e1fde4d755133d06ece
Hua Son Tung <tunghuason@gmail.com>
2021-12-11 21:15:40 +0700

    Added 2 new struct: Petition and Petitions. Create method to parse Data, fetch data and display it on tableview.

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Hua Son Tung on 11/12/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Hua Son Tung on 11/12/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
